27a371811814101355778a326b11aa0a880c3251
Dicarbene <1041641731@qq.com>
2022-03-25 12:46:20 +0800

    repo init

---
+++ README.md
@@ -0,0 +1,24 @@
+# :zap: :deciduous_tree: :pen: CUP-Treehole 石大树洞SPA
+石大树洞
+- :smiley_cat:自动化解决原石大表白墙人工工作量过大的问题
+- :robot:解决校外人员验证困难的问题
+- 响应式页面
+## Features:
+
+- :email:本校学生邮箱验证
+- :new: 时间顺序最新贴排布
+- :speech_balloon: 帖子搜索与收藏功能
+
+## :zap:技术栈: 
+### 前端:
+[Vue](https://vuejs.org/)的全家桶 (https://vuejs.org/)
+Vue3 + Vite + Pinia + Vue Router + Tailwindcss
+### 后端:
+[Node.js](https://nodejs.org/) (前端后端一把梭)
+Express.js + mySQL + Sequelize
+## :shell: 项目构建指南
+``` shell
+npm install
+npm run dev
+```
+made with :blue_heart: by wxj&zzz
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+import AsideT from './components/AsideT.vue'
+import FooterT from './components/FooterT.vue'
+import SectionT from './components/SectionT.vue'
+import NavT from './components/NavT.vue'
+
+let counter = ref(0)
+
+</script>
+
+<template>
+  <div class="dark:bg-slate-900 flex flex-col">
+    <header class=" shadow-white-100 flex flex-row">
+      <div class="mr-auto px-3 py-1 sm:px-6 lg:px-8">
+        <h1
+          class="text-3xl font-mono font-extrabold leading-loose text-slate-900 dark:text-white"
+        >Treehole</h1>
+      </div>
+    </header>
+    <main class="flex flex-row">
+      <NavT />
+      <SectionT />
+      <AsideT />
+    </main>
+    <FooterT />
+  </div>
+</template>
