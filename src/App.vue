52ccf2fb992f9e6c2dfa5a8df10a05c884d83686
Dicarbene <1041641731@qq.com>
2022-03-25 22:18:45 +0800

    little updated

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,12 @@ import FooterT from './components/FooterT.vue'
 import SectionT from './components/SectionT.vue'
 import NavT from './components/NavT.vue'
 
-let counter = ref(0)
 
 </script>
 
 <template>
   <div class="dark:bg-slate-900 flex flex-col">
-    <header class=" shadow-white-100 flex flex-row">
+    <header class="bg-blur shadow-white-100 flex flex-row">
       <div class="mr-auto px-3 py-1 sm:px-6 lg:px-8">
         <h1
           class="text-3xl font-mono font-extrabold leading-loose text-slate-900 dark:text-white"
--- src/components/SectionT.vue
+++ src/components/SectionT.vue
@@ -1,3 +1,10 @@
 <template>
-    <p class="text-white">test</p>
+    <article
+        class="flex text-slate-700 text-mono border-2 border-white ml-4 mr-auto bg-gradient-to-r from-amber-100 to-fuchsia-50"
+    >
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam
+        possimus, placeat voluptas maxime laudantium earum soluta, necessitatibus
+        sit consequatur nisi similique ipsum! Perferendis, est soluta! Culpa
+        perferendis dolorum id cupiditate!
+    </article>
 </template>
