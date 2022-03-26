6a31d05097f3f7eba7b7335e3a2794808f2cdaed
Dicarbene <1041641731@qq.com>
2022-03-26 22:02:32 +0800

    updated slightly

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,9 @@
 <script setup>
-import { ref } from 'vue'
-import AsideT from './components/AsideT.vue'
-import FooterT from './components/FooterT.vue'
-import SectionT from './components/SectionT.vue'
-import NavT from './components/NavT.vue'
-
-
+import HeaderT from './components/HeaderT.vue'
 </script>
-
 <template>
   <div class="dark:bg-slate-900 flex flex-col">
-    <header class="bg-blur shadow-white-100 flex flex-row">
-      <div class="mr-auto px-3 py-1 sm:px-6 lg:px-8">
-        <h1
-          class="text-3xl font-mono font-extrabold leading-loose text-slate-900 dark:text-white"
-        >Treehole</h1>
-      </div>
-    </header>
-    <main class="flex flex-row">
-      <NavT />
-      <SectionT />
-      <AsideT />
-    </main>
-    <FooterT />
+    <headerT />
+    <router-view></router-view>
   </div>
 </template>
---
+++ src/components/HeaderT.vue
@@ -0,0 +1,10 @@
+<template>
+  <header>
+    <div class="mr-auto px-3 py-1 sm:px-6 lg:px-8 flex flex-row">
+      <img src="public\favicon.ico" loading="lazy"/>
+      <h1
+        class="text-2xl font-sans font-extrabold leading-loose text-slate-900 dark:text-white"
+      >Treehole</h1>
+    </div>
+  </header>
+</template>
