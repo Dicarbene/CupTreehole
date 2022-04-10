904784bd8c30dbbea27f7d40919db1d18a28b969
Dicarbene <1041641731@qq.com>
2022-04-10 22:20:15 +0800

    Static page updated
    Scroll button implemented.
    Home page rewrote.

--- src/components/NavT.vue
+++ src/components/NavT.vue
@@ -1,10 +1,24 @@
+<script setup>
+  const backToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+  const backToBottom = () => {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: 'smooth'
+    })
+  }
+</script>
 <template>
   <ul class="menu bg-base-100 w-24 rounded-box">
     <li>
-      <a>Top</a>
+      <a @click="backToTop()">Top</a>
     </li>
     <li>
-      <a>Bottom</a>
+      <a @click="backToBottom()">Bottom</a>
     </li>
   </ul>
 </template>
